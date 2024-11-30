d3fdf66a68e1f101fdd6c94cae5cd3b31cdf18a9
Muhammed Köle <muhammedkole18@gmail.com>
2024-11-30 18:06:18 +0300

    Altın fiyatları eklendi.

--- money.py
+++ money.py
@@ -10,64 +10,103 @@ def para(num = 0):
         }
 
 
-    if num == 0:
-        conn.request("GET", f"/economy/currencyToAll?int={num}&base=USD", headers=headers)
+    def mny():
+        if num == 0:
+            conn.request("GET", f"/economy/currencyToAll?int={num}&base=USD", headers=headers)
 
+            res = conn.getresponse()
+            data = res.read()
+            data = data.decode("utf-8")
+            dolar = json.loads(data)
+
+            if str(dolar['success']) == 'True':
+                result = ""
+                result += f"1$ = {(dolar['result']['data'][-4]['rate'])}₺\n"
+
+            else:
+                return "Bugünlük API hakkı dolmuştur."
+
+
+            conn.request("GET", f"/economy/currencyToAll?int={num}&base=EUR", headers=headers)
+
+            res = conn.getresponse()
+            data = res.read()
+            data = data.decode("utf-8")
+            euro = json.loads(data)
+
+            if str(euro['success']) == 'True':
+                result += f"1€ = {(euro['result']['data'][-5]['rate'])}₺\n"
+
+            else:
+                return "Bugünlük API hakkı dolmuştur."
+
+            conn.request("GET", f"/economy/currencyToAll?int={num}&base=RUB", headers=headers)
+
+            res = conn.getresponse()
+            data = res.read()
+            data = data.decode("utf-8")
+            ruble = json.loads(data)
+
+            if str(ruble['success']) == 'True':
+                result += f"1₽ = {str(ruble['result']['data'][-5]['rate'])}₺"
+
+            else:
+                return "Bugünlük API hakkı dolmuştur."
+
+
+        else:
+            conn.request("GET", f"/economy/currencyToAll?int={num}&base=USD", headers=headers)
+
+            res = conn.getresponse()
+            data = res.read()
+            data = data.decode("utf-8")
+            usd = json.loads(data)
+
+            if str(usd['success']) == 'True':
+                result = ""
+                result += f"{num}$ = {(usd['result']['data'][-4]['calculatedstr'])}₺\n"
+
+            else:
+                return "Bugünlük API hakkı dolmuştur."
+
+            conn.request("GET", f"/economy/currencyToAll?int={num}&base=EUR", headers=headers)
+
+            res = conn.getresponse()
+            data = res.read()
+            data = data.decode("utf-8")
+            eur = json.loads(data)
+
+            if str(eur['success']) == 'True':
+                result += f"{num}€ = {(eur['result']['data'][-5]['calculatedstr'])}₺\n"
+
+            else:
+                return "Bugünlük API hakkı dolmuştur."
+
+            conn.request("GET", f"/economy/currencyToAll?int={num}&base=RUB", headers=headers)
+
+            res = conn.getresponse()
+            data = res.read()
+            data = data.decode("utf-8")
+            rub = json.loads(data)
+
+            if str(rub['success']) == 'True':
+                result += f"{num}₽ = {str(rub['result']['data'][-5]['calculatedstr'])}₺\n\n"
+
+            else:
+                return "Bugünlük API hakkı dolmuştur."
+    
+
+    def gold():
+
+        conn.request("GET", "/economy/goldPrice", headers=headers)  
         res = conn.getresponse()
         data = res.read()
-        data = data.decode("utf-8")
         veri = json.loads(data)
 
-        result = ""
-        result += f"1$ = {(veri['result']['data'][-4]['rate'])}₺\n"
+        for i in range(6):
+            result += f"{veri['result'][i]['name']}\nAlış: {veri['result'][i]['buying']}\nSatış: {veri['result'][i]['selling']}\n"
 
-        conn.request("GET", f"/economy/currencyToAll?int={num}&base=EUR", headers=headers)
 
-        res = conn.getresponse()
-        data = res.read()
-        data = data.decode("utf-8")
-        veri = json.loads(data)
-
-        result += f"1€ = {(veri['result']['data'][-5]['rate'])}₺\n"
-
-        conn.request("GET", f"/economy/currencyToAll?int={num}&base=RUB", headers=headers)
-
-        res = conn.getresponse()
-        data = res.read()
-        data = data.decode("utf-8")
-        veri = json.loads(data)
-
-        result += f"1₽ = {str(veri['result']['data'][-5]['rate'])}₺"
-        return result
-
-    else:
-        conn.request("GET", f"/economy/currencyToAll?int={num}&base=USD", headers=headers)
-
-        res = conn.getresponse()
-        data = res.read()
-        data = data.decode("utf-8")
-        veri = json.loads(data)
-
-        result = ""
-        result += f"{num}$ = {(veri['result']['data'][-4]['calculatedstr'])}₺\n"
-
-        conn.request("GET", f"/economy/currencyToAll?int={num}&base=EUR", headers=headers)
-
-        res = conn.getresponse()
-        data = res.read()
-        data = data.decode("utf-8")
-        veri = json.loads(data)
-
-        result += f"{num}€ = {(veri['result']['data'][-5]['calculatedstr'])}₺\n"
-
-        conn.request("GET", f"/economy/currencyToAll?int={num}&base=RUB", headers=headers)
-
-        res = conn.getresponse()
-        data = res.read()
-        data = data.decode("utf-8")
-        veri = json.loads(data)
-
-        result += f"{num}₽ = {str(veri['result']['data'][-5]['calculatedstr'])}₺"
-        return result
-
-para(10)
+    mny()
+    gold()
+para()
